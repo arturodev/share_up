6e36ddb0b1ee7dd014770da311d56889fe4cf832
arturo resendiz <arturoa.resendiz@gmail.com>
2015-08-25 16:13:43 -0500

    se agrego el esquema

--- app/controllers/esquemas_controller.rb
+++ app/controllers/esquemas_controller.rb
@@ -1,15 +1,21 @@
-class EsquemasController < ApplicationController
+class EsquemasController < InheritedResources::Base
   def index
   end
 
   def show
+    @esquema = Esquema.find_by_id(params[:id])
+     @database = @esquema.database
   end
 
   def edit
   end
 
   def delete
+    @esquema = Esquema.find_by_id(params[:id])
+    @esquema.destroy!
+    redirect_to(:controller => 'databases', :action => 'show', :id => params[:database_id])
   end
+
 
   def new
     database = Database.find_by_id(params[:id])
@@ -18,7 +24,7 @@ class EsquemasController < ApplicationController
   
   def create
     params.permit!
-    Database.create(params[:esquema])
-    redirect_to(:action => 'show', :id => @database.id)
+    x = Esquema.create(params[:esquema])
+    redirect_to(:controller => 'databases', :action => 'show', :id => x.database_id)
   end
 end
